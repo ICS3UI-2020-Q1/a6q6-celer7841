76e93dc2c5a0c206394ed0215cb116f4282d982c
celer7841 <celer7841@wrdsb.ca>
2020-10-14 05:12:32 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("What Fibonacci number would you like to find?");
+
+    int userNumber = input.nextInt();
+    int[] numbers = new int [userNumber];
+
+    numbers[0] = 1;
+    numbers[1] = 1;
+
+    for(int i=2;i<userNumber;i++){
+      numbers[i] = numbers[i-1] + numbers[i-2];
+    }
+
+
+    System.out.println("The " + userNumber + " Fibonacci number is " + numbers[userNumber -1]);
   }
 }
